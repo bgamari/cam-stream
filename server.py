22b9cdbec966ab0dda2df93634a4db552889e954
Ben Gamari <bgamari.foss@gmail.com>
2013-04-08 21:04:00 -0400

    HTTP Streaming nearly works

--- server.py
+++ server.py
@@ -2,21 +2,103 @@ import gi
 gi.require_version('Gst', '1.0')
 from gi.repository import GObject, Gst, GstApp
 from stream import Stream
+from Queue import Queue
 import threading
+from wsgiref.simple_server import WSGIServer, make_server, WSGIRequestHandler
+from SocketServer import ThreadingMixIn
 
 GObject.threads_init()
 Gst.init(None)
 
+class MyWSGIServer(ThreadingMixIn, WSGIServer):
+     pass
+
+def create_server(host, port, app, server_class=MyWSGIServer,
+          handler_class=WSGIRequestHandler):
+     return make_server(host, port, app, server_class, handler_class)
+
+INDEX_PAGE = """
+<html>
+<head>
+    <title>Gstreamer testing</title>
+</head>
+<body>
+<h1>Testing a dummy camera with GStreamer</h1>
+<img src="/mjpeg_stream"/>
+<hr />
+</body>
+</html>
+"""
+ERROR_404 = """
+<html>
+  <head>
+    <title>404 - Not Found</title>
+  </head>
+  <body>
+    <h1>404 - Not Found</h1>
+  </body>
+</html>
+"""
+
+class StreamerApp(object):
+    queues = []
+
+    def __init__(self, stream):
+        self._stream = stream
+        self._reading_stream = False
+
+    def __call__(self, environ, start_response):
+        if environ['PATH_INFO'] == '/':
+            start_response("200 OK", [
+                ("Content-Type", "text/html"),
+                ("Content-Length", str(len(INDEX_PAGE)))
+            ])
+            return iter([INDEX_PAGE])
+        elif environ['PATH_INFO'] == '/mjpeg_stream':
+            return self._start_streaming(start_response)
+        else:
+            start_response("404 Not Found", [
+                ("Content-Type", "text/html"),
+                ("Content-Length", str(len(ERROR_404)))
+            ])
+            return iter([ERROR_404])
+
+    def _start_reading(self):
+        self._stream.listen_stream(self._read_stream)
+
+    def _read_stream(self, b):
+        for q in self.queues:
+            q.put(b)
+        return len(self.queues) > 0
+
+    def _start_streaming(self, start_response):
+        start_response('200 OK', [('Content-type', 'multipart/x-mixed-replace; boundary=--break')])
+
+        self._start_reading()
+        q = Queue()
+        self.queues.append(q)
+        while True:
+            try:
+                yield q.get()
+            except:
+                print 'done'
+                if q in self.queues:
+                    self.queues.remove(q)
+                return
+
 #src = Gst.ElementFactory.make('v4l2src', 'video-source')
 src = Gst.ElementFactory.make('videotestsrc', 'video-source')
 
-def hi(b):
-    print len(b)
-    return False
-
 stream = Stream(src)
 stream.start()
-stream.listen_stream(hi)
+
+app = StreamerApp(stream)
+port = 5005
+httpd = create_server('', port, app)
+
+thrd = threading.Thread(target=httpd.serve_forever)
+thrd.daemon = True
+thrd.start()
 
 loop = GObject.MainLoop()
 loop.run()
--- stream.py
+++ stream.py
@@ -26,12 +26,14 @@ class Stream(object):
         self._stream_rate = make_element('videorate', 'stream-rate')
         self._stream_scale = make_element('videoscale', 'stream-scale')
         self._stream_enc = make_element('jpegenc', 'stream-enc')
+        self._stream_mux = make_element('multipartmux', 'stream-mux')
         self._stream_sink = make_element('appsink', 'stream-sink')
 
         #snapshot_valve = make_element('valve', 'snapshot-valve')
         #snapshot_enc = make_element('jpegenc', 'snapshot-enc')
         #snapshot_sink = make_element('appsink', 'snapshot-sink')
 
+        self._stream_mux.props.boundary = 'break'
         self._stream_valve.props.drop = False
 
         self._stream_sink.props.emit_signals = True
@@ -39,11 +41,15 @@ class Stream(object):
 
         src.link(self._tee)
 
+        caps = Gst.Caps.new_empty_simple('video/x-rgb')
+        caps.set_value('framerate', 10)
+
         self._tee.link(self._stream_valve)
         self._stream_valve.link(self._stream_rate)
         self._stream_rate.link(self._stream_scale)
         self._stream_scale.link(self._stream_enc)
-        self._stream_enc.link(self._stream_sink)
+        self._stream_enc.link(self._stream_mux)
+        self._stream_mux.link(self._stream_sink)
 
         #tee.link(snapshot_valve)
         #snapshot_valve.link(snapshot_scale)
@@ -53,13 +59,13 @@ class Stream(object):
         buf = sample.get_buffer()
         b = buffer_utils.get_buffer_data(buf)
 
+        for listener in self._stream_listeners:
+            ret = listener(b)
+            if not ret:
+                self._stream_listeners.remove(listener)
+
         if len(self._stream_listeners) == 0:
             self._stream_valve.props.drop = True
-        else:
-            for listener in self._stream_listeners:
-                ret = listener(b)
-                if not ret:
-                    self._stream_listeners.remove(listener)
 
         return Gst.FlowReturn.OK
 
