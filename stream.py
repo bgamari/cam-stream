d84dc7efcbc6491d3ddafa9b206644b061b9e683
Ben Gamari <ben@smart-cactus.org>
2018-01-02 22:14:32 -0500

    It kind of works with multifdsink

--- stream.py
+++ stream.py
@@ -7,7 +7,9 @@ gi.require_version("Gst", "1.0")
 gi.require_version("GstApp", "1.0")
 from gi.repository import Gst
 from gi.repository import GLib
+from gi.repository import Gio
 from gi.repository import GstApp
+from gi.repository import GObject
 
 import aiohttp
 from aiohttp import web
@@ -16,86 +18,59 @@ import asyncio
 
 class Source(object):
     def __init__(self, pipeline_desc):
-        self.sinks = []
         desc = pipeline_desc.format(fd=1)
+        self.fds = {}
         print('pipeline: %s' % desc)
-        pipeline = Gst.Pipeline()
+        self.pipeline = Gst.Pipeline()
         self.bin = Gst.parse_bin_from_description(desc, True)
-        pipeline.add(self.bin)
-        self.sink = GstApp.AppSink()
-        pipeline.add(self.sink)
-        self.bin.link(self.sink)
-        Gst.debug_bin_to_dot_file(self.bin, Gst.DebugGraphDetails.ALL, "graph.dot")
-        self.sink.connect('new-sample', self.have_buffer)
-        #self.sink.connect('new-preroll', self.have_buffer)
-        self.bin = pipeline
-
-    def have_buffer(self, appsink):
-        sample = appsink.try_pull_sample(0)
-        buf = sample.get_buffer()
-        (result, map_info) = buf.map(Gst.MapFlags.READ)
-        assert result
-        try:
-            for q in self.sinks:
-                try:
-                    print('@')
-                    q.put_nowait(map_info.data)
-                except asyncio.queues.QueueFull as e:
-                    print("Queue filled, removing")
-                    del self.sinks[q]
-
-        finally:
-            buf.unmap(map_info)
-
-        return Gst.FlowReturn.OK
+        self.pipeline.add(self.bin)
+        self.sink = self.pipeline.get_by_name('sink')
+        self.sink.connect('client-removed', self.on_client_removed)
+        self.add_signal = GObject.signal_lookup('add', self.sink.g_type_instance.g_class.g_type)
+        Gst.debug_bin_to_dot_file(self.pipeline, Gst.DebugGraphDetails.ALL, "graph.dot")
 
     async def start(self):
-        class QueueProtocol(asyncio.Protocol):
-            def data_received(proto, d):
-                print('^')
-                for q in self.sinks:
-                    try:
-                        print('@')
-                        q.put_nowait(d)
-                    except asyncio.queues.QueueFull as e:
-                        print("Queue filled, removing")
-                        del self.sinks[d]
-
-            def eof_received(proto):
-                print("done")
-
-        self.sink.set_emit_signals(True)
-        s = self.bin.set_state(Gst.State.PLAYING)
+        s = self.pipeline.set_state(Gst.State.PLAYING)
         print(s)
         #self.transport, _ = await loop.connect_read_pipe(QueueProtocol, self.f)
 
-    def add_sink(self, queue):
-        self.sinks.append(queue)
+    def on_client_removed(self, sink, fd, status):
+        event = self.fds.get(fd)
+        if event is None:
+            print("unknown socket %d" % fd)
+        else:
+            print("removed %d" % fd)
+            event.set()
+
+    async def add_sink(self, fd):
+        self.sink.emit('add', fd)
+        event = asyncio.Event()
+        self.fds[fd] = event
+        await event.wait()
 
     def stop(self):
         self.bin.set_state(Gst.State.NULL)
 
 Gst.init(sys.argv)
-#pipeline_desc = "v4l2src device=/dev/video1 ! video/x-raw,format=YUY2,framerate=25/1 ! videoconvert ! vp8enc ! matroskamux streamable=true ! fdsink fd={fd}"
-pipeline_desc = "v4l2src device=/dev/video1 ! video/x-raw,format=YUY2,framerate=25/1 ! videoconvert ! vp8enc ! webmmux streamable=true"
-#pipeline_desc = "v4l2src do-timestamp=true device=/dev/video1 ! video/x-raw,format=YUY2,framerate=15/1 ! videoconvert ! queue ! matroskamux streamable=true"
-#pipeline_desc = "v4l2src device=/dev/video1 ! video/x-raw,format=YUY2,framerate=25/1 ! videoconvert ! xvimagesink"
+#pipeline_desc = "v4l2src device=/dev/video1 ! video/x-raw,format=YUY2,framerate=25/1 ! videoconvert ! vp8enc ! webmmux streamable=true ! multifdsink name=sink"
+pipeline_desc = "v4l2src device=/dev/video1 ! video/x-raw,format=YUY2,framerate=10/1 ! videoconvert ! vp8enc ! webmmux streamable=true ! multifdsink name=sink"
+
 src = Source(pipeline_desc)
 
 async def handle(request):
     print(request)
     resp = web.StreamResponse()
+    resp.content_length = -1
     #resp.content_type = 'multipart/x-mixed-replace: dou'
     resp.content_type = 'video/webm'
     await resp.prepare(request)
+    await resp.drain()
 
-    q = asyncio.queues.Queue()
-    src.add_sink(q)
-    while True:
-        d = await q.get()
-        resp.write(d)
-        await resp.drain()
-        print('.',)
+    fd = request.transport._sock_fd
+    print('started')
+    await src.add_sink(fd)
+    print('done')
+    return resp
 
 app = web.Application()
 app.router.add_get('/', handle)
@@ -107,7 +82,7 @@ async def glib_loop():
     while True:
         while ctx.pending():
             ctx.iteration(False)
-        await asyncio.sleep(0.01)
+        await asyncio.sleep(0.001)
 
 app.loop.create_task(glib_loop())
 
