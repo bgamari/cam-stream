3c2c6deeb4492d0816675fd5b184c3dcefebb50b
Ben Gamari <ben@smart-cactus.org>
2018-01-03 15:28:25 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='camstream',
+      version='1.0',
+      description='',
+      author='Ben Gamari',
+      author_email='ben@smart-cactus.org',
+      url='https://github.com/bgamari/camstream',
+      scripts=['stream.py']
+     )
